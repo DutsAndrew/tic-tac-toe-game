71e8bbfad518eb906004fca3ba4a085a18b230fc
Andrew Dutson <Dutsandrew@gmail.com>
2022-03-29 13:16:32 -0600

    validated form to prevent inputing a player two name when playing vs computer

--- javascript.js
+++ javascript.js
@@ -4,13 +4,28 @@ const gameForm = (function() {
     const startGameButton = document.querySelector('#start-game');
     const startGameForm = document.querySelector('#game-start-container');
     const closeGameForm = document.querySelector('#close-game-form');
+    const playerTwoName = document.querySelector('#player-two-input-container');
 
-    startGameButton.addEventListener('click', startGame);
-    closeGameForm.addEventListener('click', closeForm)
+    startGameButton.addEventListener('click', _openForm);
+    closeGameForm.addEventListener('click', _closeForm);
 
+    // Controls for when Players are being selected
+    const playerVsComputer = document.getElementById('player-vs-computer').onclick = function() {
+        console.log("Player VS Computer was selected");
+        playerTwoName.style.display = "none";
+    }
+
+    const playerVsPlayer = document.getElementById('player-vs-player').onclick = function() {
+        console.log("Player VS Player was selected");
+        if (playerTwoName.style.display = "none") {
+            playerTwoName.style.display = "flex";
+        }
+    }
+
+    // functions to open and close the form
     let gameFormOpen = false;
 
-    function startGame() {
+    function _openForm() {
         if (gameFormOpen === false) {
             console.log("start game has been clicked");
             startGameForm.style.display = "flex";
@@ -18,7 +33,7 @@ const gameForm = (function() {
         }
     }
 
-    function closeForm() {
+    function _closeForm() {
         if (gameFormOpen === true) {
             console.log("Close game form button has been clicked");
             startGameForm.style.display = "none";

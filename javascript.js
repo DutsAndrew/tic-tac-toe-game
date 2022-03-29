13740c32e34c4b5cc4a584ac934ae0b91cb3dc24
Andrew Dutson <Dutsandrew@gmail.com>
2022-03-28 19:59:45 -0600

    setup start game form and JS listener on click of start game

---
+++ javascript.js
@@ -0,0 +1,23 @@
+const startGameModule = (function() {
+    'use strict';
+
+    let startGameButton = document.querySelector('#start-game');
+    startGameButton.addEventListener('click', startGame);
+
+    function startGame() {
+        console.log("start game has been clicked");
+    }
+})();
+
+
+const playerModule = (function() {
+    'use strict';
+
+    const playerFactory = (name) => {
+        if (playerNameExists === true) {
+            playerOne
+        }
+
+    }
+
+})();

73d4ff77d88474d79fc95d128a412945f3a83944
Andrew Dutson <Dutsandrew@gmail.com>
2022-03-29 10:35:56 -0600

    set up javascript to open and close form on click

--- javascript.js
+++ javascript.js
@@ -1,11 +1,29 @@
-const startGameModule = (function() {
+const gameForm = (function() {
     'use strict';
 
-    let startGameButton = document.querySelector('#start-game');
+    const startGameButton = document.querySelector('#start-game');
+    const startGameForm = document.querySelector('#game-start-container');
+    const closeGameForm = document.querySelector('#close-game-form');
+
     startGameButton.addEventListener('click', startGame);
+    closeGameForm.addEventListener('click', closeForm)
+
+    let gameFormOpen = false;
 
     function startGame() {
-        console.log("start game has been clicked");
+        if (gameFormOpen === false) {
+            console.log("start game has been clicked");
+            startGameForm.style.display = "flex";
+            gameFormOpen = true;
+        }
+    }
+
+    function closeForm() {
+        if (gameFormOpen === true) {
+            console.log("Close game form button has been clicked");
+            startGameForm.style.display = "none";
+            gameFormOpen = false;
+        }
     }
 })();
 
